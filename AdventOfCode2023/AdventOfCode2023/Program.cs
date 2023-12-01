b55550e8163e3cca793f87e1280fd41a6d5e20a7
Marcos Martínez Játiva <marcos.martinez-jativa@capgemini.com>
2023-12-01 12:25:08 +0100

    First puzzle

--- AdventOfCode2023/AdventOfCode2023/Program.cs
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode2023.Puzzles;
+
+Console.WriteLine("Day1 Puzzle1: " + Day1.Puzzle1());
---
+++ AdventOfCode2023/AdventOfCode2023/Puzzles/Day1.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode2023.Puzzles
+{
+    public static class Day1
+    {
+        public static int Puzzle1()
+        {
+            var sum = 0;
+            var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay1Puzzle1.txt");
+            foreach (var line in input)
+            { 
+                var allNumbers = line.ToCharArray().Where(c => Char.IsDigit(c)).ToArray();
+                var thisNumber = int.Parse(new string(new char[] { allNumbers[0], allNumbers[allNumbers.Length - 1] }));
+                sum += thisNumber;
+            }
+
+            return sum;
+        }
+
+        public static int Puzzle2()
+        {
+            var sum = 0;
+            var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay1Puzzle1.txt");
+            foreach (var line in input)
+            {
+                var allNumbers = line.ToCharArray().Where(c => Char.IsDigit(c)).ToArray();
+                var thisNumber = int.Parse(new string(new char[] { allNumbers[0], allNumbers[allNumbers.Length - 1] }));
+                sum += thisNumber;
+            }
+
+            return sum;
+        }
+    }
+}

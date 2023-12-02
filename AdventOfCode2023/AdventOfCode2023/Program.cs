4fe2b3a075ad2fd2b24c25a517bc332bfd26dfaa
Marcos Martínez Játiva <marcos.martinez-jativa@capgemini.com>
2023-12-02 17:58:59 +0100

    Day2Puzzle1

--- AdventOfCode2023/AdventOfCode2023/Program.cs
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -1,4 +1,3 @@
 ﻿using AdventOfCode2023.Puzzles;
 
-Console.WriteLine("Day1 Puzzle1: " + Day1.Puzzle1());
-Console.WriteLine("Day1 Puzzle2: " + Day1.Puzzle2());
+Console.WriteLine("Day2 Puzzle1: " + Day2.Puzzle1());
---
+++ AdventOfCode2023/AdventOfCode2023/Puzzles/Day2.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode2023.Puzzles
+{
+    public class Day2
+    {
+        public static int Puzzle1()
+        {
+            var sum = 0;
+            var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay2Puzzle1.txt");
+            foreach (var line in input)
+            {
+                var myTempDictionary = new Dictionary<string, int>()
+                {
+                    { "blue", 0 },
+                    { "red", 0 },
+                    { "green", 0}
+                };
+
+                var splitId = line.Split(":");
+                var id = int.Parse(splitId[0].Split().Last().ToString());
+
+                var splitSets = splitId[1].Split(";");
+                foreach (var set in splitSets)
+                {
+                    var splitBalls = set.Split(",");
+                    foreach (var balls in splitBalls)
+                    {
+                        var splitNumberFromBall = balls.Split();
+                        var numberOfBallsShown = int.Parse(splitNumberFromBall[1]);
+                        if (numberOfBallsShown > myTempDictionary[splitNumberFromBall[2]])
+                        {
+                            myTempDictionary[splitNumberFromBall[2]] = numberOfBallsShown;
+                        }
+                    }
+                }
+
+                if (myTempDictionary["red"] <= 12 && myTempDictionary["green"] <= 13 && myTempDictionary["blue"] <= 14)
+                {
+                    sum += id;
+                }
+            }
+
+            return sum;
+        }
+    }
+}

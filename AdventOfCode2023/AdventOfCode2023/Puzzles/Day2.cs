2649f862c51d606297a4933ddeaf69e1ed09a59e
Marcos Martínez Játiva <marcos.martinez-jativa@capgemini.com>
2023-12-02 18:09:59 +0100

    Refactor of Day2 because it was cool :D

--- AdventOfCode2023/AdventOfCode2023/Puzzles/Day2.cs
+++ AdventOfCode2023/AdventOfCode2023/Puzzles/Day2.cs
@@ -8,32 +8,9 @@
             var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay2Puzzle1.txt");
             foreach (var line in input)
             {
-                var myTempDictionary = new Dictionary<string, int>()
-                {
-                    { "blue", 0 },
-                    { "red", 0 },
-                    { "green", 0}
-                };
+                var ballsOfEachColor = GetMaxBallsOfEachColor(line, out var id);
 
-                var splitId = line.Split(":");
-                var id = int.Parse(splitId[0].Split().Last().ToString());
-
-                var splitSets = splitId[1].Split(";");
-                foreach (var set in splitSets)
-                {
-                    var splitBalls = set.Split(",");
-                    foreach (var balls in splitBalls)
-                    {
-                        var splitNumberFromBall = balls.Split();
-                        var numberOfBallsShown = int.Parse(splitNumberFromBall[1]);
-                        if (numberOfBallsShown > myTempDictionary[splitNumberFromBall[2]])
-                        {
-                            myTempDictionary[splitNumberFromBall[2]] = numberOfBallsShown;
-                        }
-                    }
-                }
-
-                if (myTempDictionary["red"] <= 12 && myTempDictionary["green"] <= 13 && myTempDictionary["blue"] <= 14)
+                if (ballsOfEachColor["red"] <= 12 && ballsOfEachColor["green"] <= 13 && ballsOfEachColor["blue"] <= 14)
                 {
                     sum += id;
                 }
@@ -48,34 +25,42 @@
             var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay2Puzzle2.txt");
             foreach (var line in input)
             {
-                var myTempDictionary = new Dictionary<string, int>()
-                {
-                    { "blue", 0 },
-                    { "red", 0 },
-                    { "green", 0}
-                };
+                var ballsOfEachColor = GetMaxBallsOfEachColor(line, out _);
 
-                var splitFromId = line.Split(":");
-
-                var splitSets = splitFromId[1].Split(";");
-                foreach (var set in splitSets)
-                {
-                    var splitBalls = set.Split(",");
-                    foreach (var balls in splitBalls)
-                    {
-                        var splitNumberFromBall = balls.Split();
-                        var numberOfBallsShown = int.Parse(splitNumberFromBall[1]);
-                        if (numberOfBallsShown > myTempDictionary[splitNumberFromBall[2]])
-                        {
-                            myTempDictionary[splitNumberFromBall[2]] = numberOfBallsShown;
-                        }
-                    }
-                }
-
-                sum += myTempDictionary["red"] * myTempDictionary["green"] * myTempDictionary["blue"];
+                sum += ballsOfEachColor["red"] * ballsOfEachColor["green"] * ballsOfEachColor["blue"];
             }
 
             return sum;
         }
-    }
+
+        public static Dictionary<string, int> GetMaxBallsOfEachColor(string line, out int id)
+        {
+            var result = new Dictionary<string, int>()
+            {
+                { "blue", 0 },
+                { "red", 0 },
+                { "green", 0 }
+            };
+
+            var splitFromId = line.Split(":");
+            id = int.Parse(splitFromId[0].Split().Last().ToString());
+
+            var splitSets = splitFromId[1].Split(";");
+            foreach (var set in splitSets)
+            {
+                var splitBalls = set.Split(",");
+                foreach (var balls in splitBalls)
+                {
+                    var splitNumberFromBall = balls.Split();
+                    var numberOfBallsShown = int.Parse(splitNumberFromBall[1]);
+                    if (numberOfBallsShown > result[splitNumberFromBall[2]])
+                    {
+                        result[splitNumberFromBall[2]] = numberOfBallsShown;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }    
 }

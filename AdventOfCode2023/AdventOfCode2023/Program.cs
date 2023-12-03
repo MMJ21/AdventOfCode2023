39d1f156c8a99f705ba277eda371a2c1fe7cecff
Marcos Martínez Játiva <marcos.martinez-jativa@capgemini.com>
2023-12-03 13:10:13 +0100

    Day3Puzzle2

--- AdventOfCode2023/AdventOfCode2023/Program.cs
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -1,3 +1,4 @@
 ﻿using AdventOfCode2023.Puzzles;
 
 Console.WriteLine("Day3 Puzzle1: " + Day3.Puzzle1());
+Console.WriteLine("Day3 Puzzle2: " + Day3.Puzzle2());
--- AdventOfCode2023/AdventOfCode2023/Puzzles/Day3.cs
+++ AdventOfCode2023/AdventOfCode2023/Puzzles/Day3.cs
@@ -40,6 +40,30 @@
             return sum;
         }
 
+        public static int Puzzle2()
+        {
+            var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay3Puzzle2.txt");
+            var sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentLine = input[i];
+                var previousLine = i != 0 ? input[i - 1] : string.Empty;
+                var nextLine = i + 1 < input.Length ? input[i + 1] : string.Empty;
+
+                for (int j = 0; j < currentLine.Length; j++)
+                {
+                    var character = currentLine[j];
+                    if (character == '*')
+                    {
+                        sum += AddIfGearRatio(currentLine, previousLine, nextLine, j);
+                    }
+                }
+            }
+
+            return sum;
+        }
+
         public static int AddIfPartNumber(string currentLine, string previousLine, string nextLine, string myNumber, int iterator)
         {
             var isPartNumber = false;
@@ -72,6 +96,137 @@
             }
 
             if (isPartNumber) { return int.Parse(myNumber); }
+            else { return 0; }
+        }
+
+        public static int AddIfGearRatio(string currentLine, string previousLine, string nextLine, int iterator)
+        {
+            var firstAdjacent = string.Empty;
+            var secondAdjacent = string.Empty;
+
+            var numberInPreviousLine = string.Empty;
+            if (!string.IsNullOrEmpty(previousLine))
+            {
+                for (int i = 0; i < previousLine.Length; i++)
+                {
+                    if (char.IsDigit(previousLine[i]))
+                    {
+                        numberInPreviousLine += previousLine[i];
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(numberInPreviousLine)) { continue; }
+                        if (iterator >= i - numberInPreviousLine.Length - 1 && iterator <= i)
+                        {
+                            if (string.IsNullOrEmpty(firstAdjacent))
+                            {
+                                firstAdjacent = numberInPreviousLine;
+                            }
+                            else
+                            {
+                                secondAdjacent = numberInPreviousLine;
+                                break;
+                            }
+                        }
+
+                        numberInPreviousLine = string.Empty;
+                    }
+                }
+                if (!string.IsNullOrEmpty(numberInPreviousLine))
+                {
+                    if (iterator >= previousLine.Length - numberInPreviousLine.Length - 1 && iterator <= previousLine.Length)
+                    {
+                        firstAdjacent = numberInPreviousLine;
+                    }
+                }
+            }
+
+            var numberInCurrentLine = string.Empty;
+            if (string.IsNullOrEmpty(secondAdjacent))
+            {
+                for (int i = 0; i < currentLine.Length; i++)
+                {
+                    if (char.IsDigit(currentLine[i]))
+                    {
+                        numberInCurrentLine += currentLine[i];
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(numberInCurrentLine)) { continue; }
+                        if (iterator >= i - numberInCurrentLine.Length - 1 && iterator <= i)
+                        {
+                            if (string.IsNullOrEmpty(firstAdjacent))
+                            {
+                                firstAdjacent = numberInCurrentLine;
+                            }
+                            else
+                            {
+                                secondAdjacent = numberInCurrentLine;
+                                break;
+                            }
+                        }
+
+                        numberInCurrentLine = string.Empty;
+                    }
+                }
+                if (!string.IsNullOrEmpty(numberInCurrentLine))
+                {
+                    if (iterator >= currentLine.Length - numberInCurrentLine.Length - 1 && iterator <= currentLine.Length)
+                    {
+                        if (string.IsNullOrEmpty(firstAdjacent))
+                        {
+                            firstAdjacent = numberInCurrentLine;
+                        }
+                        else
+                        {
+                            secondAdjacent = numberInCurrentLine;
+                        }
+                    }
+                }
+            }
+            
+
+            var numberInNextLine = string.Empty;
+            if (string.IsNullOrEmpty(secondAdjacent) && !string.IsNullOrEmpty(nextLine))
+            {
+                for (int i = 0; i < nextLine.Length; i++)
+                {
+                    if (char.IsDigit(nextLine[i]))
+                    {
+                        numberInNextLine += nextLine[i];
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(numberInNextLine)) { continue; }
+                        if (iterator >= i - numberInNextLine.Length - 1 && iterator <= i)
+                        {
+                            if (string.IsNullOrEmpty(firstAdjacent))
+                            {
+                                firstAdjacent = numberInNextLine;
+                            }
+                            else
+                            {
+                                secondAdjacent = numberInNextLine;
+                                break;
+                            }
+                        }
+
+                        numberInNextLine = string.Empty;
+                    }
+                }
+                if (!string.IsNullOrEmpty(numberInNextLine))
+                {
+                    if (iterator >= nextLine.Length - numberInNextLine.Length - 1 && iterator <= nextLine.Length)
+                    {
+                        secondAdjacent = numberInNextLine;
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(firstAdjacent) && !string.IsNullOrEmpty(secondAdjacent))
+            {
+                return int.Parse(firstAdjacent) * int.Parse(secondAdjacent);
+            }
             else { return 0; }
         }
     }

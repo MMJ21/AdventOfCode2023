4c24f9421ec84727de472e3e4a4ba2d8bdea42ad
Marcos Martínez Játiva <marcos.martinez-jativa@capgemini.com>
2023-12-04 16:15:38 +0100

    Functional Programming Methods and Comparison

--- AdventOfCode2023/AdventOfCode2023/Program.cs
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -1,4 +1,6 @@
 ﻿using AdventOfCode2023.Puzzles;
 
 Console.WriteLine("Day4 Puzzle1: " + Day4.Puzzle1());
+Console.WriteLine("Day4 Puzzle2 Functional: " + Day4.Puzzle1Functional());
 Console.WriteLine("Day4 Puzzle2: " + Day4.Puzzle2());
+Console.WriteLine("Day4 Puzzle2 Functional: " + Day4.Puzzle2Functional());
--- AdventOfCode2023/AdventOfCode2023/Puzzles/Day4.cs
+++ AdventOfCode2023/AdventOfCode2023/Puzzles/Day4.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace AdventOfCode2023.Puzzles
 {
@@ -10,6 +6,7 @@ namespace AdventOfCode2023.Puzzles
     {
         public static int Puzzle1()
         {
+            var stopWatch = Stopwatch.StartNew();
             var sum = 0;
             var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay4Puzzle1.txt");
 
@@ -30,10 +27,41 @@ namespace AdventOfCode2023.Puzzles
                     .Select(x => int.TryParse(x, out var n) ? n : (int?)null)
                     .Where(n => n.HasValue && winningNumbers.Contains(n.Value)).Count() - 1);
             }
+
+            Console.WriteLine("Imperative: " + stopWatch.ElapsedMilliseconds);
             return sum;
         }
+
+        public static int Puzzle1Functional()
+        {
+            var stopWatch = Stopwatch.StartNew();
+            var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay4Puzzle1.txt");
+
+            var sum = input.Select(line =>
+            {
+                var splitCards = line.Split(':')[1].Split("|");
+                var winnersCard = splitCards[0];
+                var myCard = splitCards[1];
+
+                var winningNumbers = winnersCard.Split(' ')
+                    .Select(x => int.TryParse(x, out var n) ? n : (int?)null)
+                    .Where(n => n.HasValue)
+                    .Select(n => n.Value)
+                    .ToList();
+
+                return (int)Math.Pow(2,
+                    myCard.Split(' ')
+                    .Select(x => int.TryParse(x, out var n) ? n : (int?)null)
+                    .Count(n => n.HasValue && winningNumbers.Contains(n.Value)) - 1);
+            }).Sum();
+
+            Console.WriteLine("Functional: " + stopWatch.ElapsedMilliseconds);
+            return sum;
+        }
+
         public static int Puzzle2()
         {
+            var stopWatch = Stopwatch.StartNew();
             var sum = 0;
             var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay4Puzzle2.txt");
             Dictionary<int, int> cardsPerGame = new Dictionary<int, int>();
@@ -53,15 +81,10 @@ namespace AdventOfCode2023.Puzzles
                     .Where(n => n.HasValue)
                     .Select(n => n.Value)
                     .ToList();
-                var addedCards = 0;
 
-                foreach (var number in myCard.Split(' '))
-                {
-                    if (int.TryParse(number, out var myNumber) && winningNumbers.Contains(myNumber))
-                    {
-                        addedCards++;
-                    }
-                }
+                var addedCards = myCard.Split(' ')
+                    .Select(x => int.TryParse(x, out var n) ? n : (int?)null)
+                    .Where(n => n.HasValue && winningNumbers.Contains(n.Value)).Count();
 
                 for (int k = 1; k <= addedCards; k++)
                 {
@@ -70,6 +93,45 @@ namespace AdventOfCode2023.Puzzles
 
                 sum += cardsPerGame[j+1];
             }
+
+            Console.WriteLine("Imperative: " + stopWatch.ElapsedMilliseconds);
+            return sum;
+        }
+
+        public static int Puzzle2Functional()
+        {
+            var stopWatch = Stopwatch.StartNew();
+            var input = File.ReadAllLines(Consts.MyInputPath + @"\inputDay4Puzzle2.txt");
+
+            var cardsPerGame = Enumerable.Range(1, input.Length)
+                .ToDictionary(i => i, _ => 1);
+
+            var sum = input.Select((line, j) =>
+            {
+                var splitCards = line.Split(':')[1].Split("|");
+                var winnersCard = splitCards[0];
+                var myCard = splitCards[1];
+
+                var winningNumbers = winnersCard.Split(' ')
+                    .Select(x => int.TryParse(x, out var n) ? n : (int?)null)
+                    .Where(n => n.HasValue)
+                    .Select(n => n.Value)
+                    .ToList();
+
+                var addedCards = myCard.Split(' ')
+                    .Select(x => int.TryParse(x, out var n) ? n : (int?)null)
+                    .Where(n => n.HasValue && winningNumbers.Contains(n.Value))
+                    .Count();
+
+                cardsPerGame = cardsPerGame
+                    .ToDictionary(entry => entry.Key,
+                        entry => entry.Key > j + 1 && entry.Key <= j + addedCards + 1
+                            ? entry.Value + cardsPerGame[j + 1]
+                            : entry.Value);
+
+                return cardsPerGame[j + 1];
+            }).Sum();
+            Console.WriteLine("Functional: " + stopWatch.ElapsedMilliseconds);
             return sum;
         }
     }

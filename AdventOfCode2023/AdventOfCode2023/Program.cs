f4b217d6c37c1c12505e92d701ce101555f31778
Marcos Martínez Játiva <marmarj2@inf.upv.es>
2023-12-05 12:59:12 +0100

    Day 5 Puzzle 2 (get me out)

--- AdventOfCode2023/AdventOfCode2023/Program.cs
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -1,3 +1,4 @@
 ﻿using AdventOfCode2023.Puzzles;
 
 Console.WriteLine("Day5 Puzzle1: " + Day5.Puzzle1());
+Console.WriteLine("Day5 Puzzle2: " + Day5.Puzzle2());
--- AdventOfCode2023/AdventOfCode2023/Puzzles/Day5.cs
+++ AdventOfCode2023/AdventOfCode2023/Puzzles/Day5.cs
@@ -52,5 +52,83 @@
 
             return res;
         }
+
+        public static long Puzzle2()
+        {
+            var input = File.ReadAllLines(Consts.MyInputPath + "/inputDay5Puzzle2.txt");
+
+            var seeds = input[0].Split().Skip(1).Select(seed => long.Parse(seed)).ToList();
+            var myMaps = new List<List<(long StartRangeSource, long EndRangeSource, long AdjustmentDestination)>>();
+            List<(long StartRangeSource, long EndRangeSource, long AdjustmentDestination)> currentMap = null;
+            foreach (var line in input.Skip(2))
+            {
+                if (line.EndsWith(":"))
+                {
+                    currentMap = new List<(long StartRangeSource, long EndRangeSource, long AdjustmentDestination)>();
+                }
+                else if (line.Length != 0)
+                {
+                    var numbers = line.Split().Select(number => long.Parse(number)).ToArray();
+                    currentMap!.Add((numbers[1], numbers[1] + numbers[2] - 1, numbers[0] - numbers[1]));
+                }
+                else if (currentMap != null)
+                {
+                    myMaps.Add(currentMap);
+                    currentMap = null;
+                }
+            }
+            if (currentMap != null)
+            {
+                myMaps.Add(currentMap);
+            }
+
+            var seedRanges = new List<(long StartRangeSeed, long EndRangeSeed)>();
+            for (int i = 0; i < seeds.Count; i += 2)
+            {
+                seedRanges.Add((seeds[i], seeds[i] + seeds[i + 1] - 1));
+            }
+
+            foreach (var map in myMaps)
+            {
+                var orderedMap = map.OrderBy(x => x.StartRangeSource).ToList();
+
+                var currentRanges = new List<(long StartRangeCurrent, long EndRangeCurrent)>();
+                foreach (var range in seedRanges)
+                {
+                    var currentRange = range;
+
+                    foreach (var mapRange in orderedMap)
+                    {
+                        if (currentRange.StartRangeSeed < mapRange.StartRangeSource)
+                        {
+                            currentRanges.Add((currentRange.StartRangeSeed, Math.Min(currentRange.EndRangeSeed, mapRange.StartRangeSource - 1)));
+                            currentRange.StartRangeSeed = mapRange.StartRangeSource;
+                            if (currentRange.StartRangeSeed > currentRange.EndRangeSeed)
+                            {
+                                break;
+                            }
+                        }
+                        if (currentRange.StartRangeSeed <= mapRange.EndRangeSource)
+                        {
+                            currentRanges.Add((currentRange.StartRangeSeed + mapRange.AdjustmentDestination, Math.Min(currentRange.EndRangeSeed, mapRange.EndRangeSource) + mapRange.AdjustmentDestination));
+                            currentRange.StartRangeSeed = mapRange.EndRangeSource + 1;
+                            if (currentRange.StartRangeSeed > currentRange.EndRangeSeed)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (currentRange.StartRangeSeed <= currentRange.EndRangeSeed)
+                    {
+                        currentRanges.Add(currentRange);
+                    }
+                }
+
+                seedRanges = currentRanges;
+            }
+
+            return seedRanges.Min(r => r.StartRangeSeed);
+        }
     }
 }
